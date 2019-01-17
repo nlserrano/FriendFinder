080f76d195ed9913c96c61efa4734428c8e78d25
Nathan <nathanlserrano@gmail.com>
2019-01-17 12:21:15 -0800

    html docss mostly finished

---
+++ app/data/friends.js
@@ -0,0 +1,134 @@
+// Array for storing friend data
+var friendsArray = [
+	{
+		name: "Nathan Drake",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+	},
+	{
+		name: "Koji Pro",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+	},
+	{
+		name: "Lionus",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+	},
+	{
+		name: "Leon Kennedy",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+	},
+	{
+		name: "Kirby",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+	},
+	{
+		name: "Raiden",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+	},
+	{
+		name: "Robot",
+		photo: "",
+		scores: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+		]
+    },
+    {
+        name: "Sam Bridges",
+        photo: "",
+        scores: [
+            "",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""
+        ]
+    }
+
+];
+
+module.exports = friendsArray;
