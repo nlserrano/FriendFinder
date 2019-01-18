93a39d81cc2883b33a057a4093a176075081eb39
Nathan <nathanlserrano@gmail.com>
2019-01-17 20:58:43 -0800

    Deploying to heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("FriendFinder app listening on PORT: " + PORT);
 });
 
 
@@ -38,26 +38,3 @@ app.listen(PORT, function() {
 
 // // Expose the public directory to access CSS files
 // app.use(express.static(path.join(__dirname, './app/public')));
-
-// // 
-// // from bill
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use('/public', express.static(path.join(__dirname , 'app/public')));
-// // 
-// // 
-
-// // Using body-parser module
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-
-// // Add the application routes
-// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-// require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
-
-// // Start listening on PORT
-// app.listen(PORT, function() {
-//   console.log('Friend Finder app is listening on PORT: ' + PORT);
-// }):
